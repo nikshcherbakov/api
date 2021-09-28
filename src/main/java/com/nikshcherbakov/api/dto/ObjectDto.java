d8abd9f318959db36fcf4ec2c9574f703f5cc263
nikshcherbakov <shcerbackow@yandex.ru>
2021-09-29 01:21:26 +0300

    Added NoArgsConstructor to pojo

--- src/main/java/com/nikshcherbakov/api/dto/ObjectDto.java
+++ src/main/java/com/nikshcherbakov/api/dto/ObjectDto.java
@@ -2,9 +2,11 @@ package com.nikshcherbakov.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ObjectDto {
     private String name;
     private String value;
